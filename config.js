758780ee323ee3ac9c3ae60e6e92ee7696bfe414
Ahmad Shah Ghaznawi <32619749+aghaznaw@users.noreply.github.com>
2023-10-27 11:52:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"database" : "mongodb://ahmadshah:aqszawsx2020@ds225382.mlab.com:25382/userstorymini",
+	"database" : "mongodb://ahmadshah:xxxx@ds225382.mlab.com:25382/userstorymini",
 	"port":  3000,
 	"secretKey": "aqszawsx",
 
